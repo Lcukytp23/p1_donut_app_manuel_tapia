95baba7c7bdecb79f8e0fc2da133b09ea8c1bdd7
Lcukytp23 <manueltapiapuc15@gmail.com>
2024-09-05 11:05:52 -0600

    AppBar added

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+      backgroundColor: const Color.fromARGB(255, 2, 40, 71),
+      leading: const Icon(
+        Icons.menu, 
+        color: Colors.white,)
+      ),
+    );
+  }
+}
