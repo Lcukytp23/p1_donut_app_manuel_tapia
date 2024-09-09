cbe72e0be449d78f77fcc8ec5e48580cfc5fe8da
Lcukytp23 <manueltapiapuc15@gmail.com>
2024-09-09 13:14:05 -0600

    myTab añadido

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/my_tab.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -8,14 +10,67 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List<Widget> myTabs = [
+    //Donut tab
+    const MyTab(
+      iconPath: "lib/icons/donut.png",
+      ),
+    //burger tab
+    const MyTab(
+      iconPath: "lib/icons/burger.png",
+      ),
+    //smoothie tab
+    const MyTab(
+      iconPath: "lib/icons/smoothie.png",
+      ),
+    //pancake tab
+    const MyTab(
+      iconPath: "lib/icons/pancakes.png",
+      ),
+    //pizza tab
+    const MyTab(iconPath: "lib/icons/pizza.png",)
+  ];
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-      backgroundColor: const Color.fromARGB(255, 2, 40, 71),
-      leading: const Icon(
-        Icons.menu, 
-        color: Colors.white,)
+    return  DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Icon(
+            Icons.menu, 
+            color:Colors.grey[800],
+            ),
+        ),
+        actions: const [Padding(
+          padding: EdgeInsets.only(right: 24.0),
+          child: Icon(Icons.person),
+        )],
+        ),
+        body:  Column(
+      children: [
+        // Texto "I want to eat"
+        const Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Row(
+            children: [
+              Text("I want to ", style: TextStyle(fontSize: 24,),),
+              Text("Eat", style: TextStyle(fontSize: 24, 
+              fontWeight: FontWeight.bold, 
+              decoration: TextDecoration.underline)
+              ), 
+            ],
+          ),
+        ), 
+        //Tab bar
+        TabBar(tabs: myTabs),
+        //Tab bar view
+        //
+        //Total del carrito      
+      ],),
       ),
     );
   }
